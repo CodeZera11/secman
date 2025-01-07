84209e19182eca33f5ebd7ad7c90e0d7d44c0541
codezera <codezera3@gmail.com>
2025-01-07 13:43:05 +0530

    added common tailwing config to separate package

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,15 +1,76 @@
 import type { Config } from "tailwindcss";
+// import TailwindCssAnimate from "tailwindcss-animate";
+import config from "@repo/ui/tailwind.config";
 
-// We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
-  theme: {
-    extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+const shadcnConfig = {
+  ...config,
+  presets: [config],
+  // darkMode: ["class"],
+  // content: [
+  //   "./pages/**/*.{ts,tsx}",
+  //   "./components/**/*.{ts,tsx}",
+  //   "./app/**/*.{ts,tsx}",
+  //   "./src/**/*.{ts,tsx}",
+  //   "../../packages/ui/src/**/*.{ts,tsx}",
+  // ],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       border: "hsl(var(--border))",
+  //       input: "hsl(var(--input))",
+  //       ring: "hsl(var(--ring))",
+  //       background: "hsl(var(--background))",
+  //       foreground: "hsl(var(--foreground))",
+  //       primary: {
+  //         DEFAULT: "hsl(var(--primary))",
+  //         foreground: "hsl(var(--primary-foreground))",
+  //       },
+  //       secondary: {
+  //         DEFAULT: "hsl(var(--secondary))",
+  //         foreground: "hsl(var(--secondary-foreground))",
+  //       },
+  //       destructive: {
+  //         DEFAULT: "hsl(var(--destructive))",
+  //         foreground: "hsl(var(--destructive-foreground))",
+  //       },
+  //       muted: {
+  //         DEFAULT: "hsl(var(--muted))",
+  //         foreground: "hsl(var(--muted-foreground))",
+  //       },
+  //       accent: {
+  //         DEFAULT: "hsl(var(--accent))",
+  //         foreground: "hsl(var(--accent-foreground))",
+  //       },
+  //       popover: {
+  //         DEFAULT: "hsl(var(--popover))",
+  //         foreground: "hsl(var(--popover-foreground))",
+  //       },
+  //       card: {
+  //         DEFAULT: "hsl(var(--card))",
+  //         foreground: "hsl(var(--card-foreground))",
+  //       },
+  //     },
+  //     borderRadius: {
+  //       lg: `var(--radius)`,
+  //       md: `calc(var(--radius) - 2px)`,
+  //       sm: "calc(var(--radius) - 4px)",
+  //     },
+  //   },
+  // },
+  // plugins: [TailwindCssAnimate],
+} satisfies Config;
+
+export default shadcnConfig;
+
+// const config: Omit<Config, "content"> = {
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "glow-conic":
+//           "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
