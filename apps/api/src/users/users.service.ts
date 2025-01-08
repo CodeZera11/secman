1f6a39d7eb0322106016af5c364293c51401d03d
codezera <codezera3@gmail.com>
2025-01-08 10:39:36 +0530

    some prisma changes

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -8,7 +8,6 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   create(createUserDto: CreateUserDto) {
-    
     return 'This action adds a new user';
   }
 
--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,7 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
-export * from "@prisma/client";
+export * from "@prisma/client/edge";
