87402795eed4b3f5b1edc0a64854a097bc793874
codezera <codezera3@gmail.com>
2025-01-09 23:05:14 +0530

    integrating next auth

---
+++ apps/web/auth.ts
@@ -0,0 +1,69 @@
+import bcrypt from "bcryptjs";
+import NextAuth, { NextAuthResult } from "next-auth";
+import { prisma } from "@repo/db";
+import Github from "next-auth/providers/github";
+import { saltAndHashPassword } from "@repo/utils";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
+
+const nextAuth = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials || !credentials.email || !credentials.password) {
+          return null;
+        }
+
+        const email = credentials.email as string;
+        const hash = saltAndHashPassword(credentials.password);
+
+        let user: any = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              email,
+              hashedPassword: hash,
+            },
+          });
+        } else {
+          const isMatch = bcrypt.compareSync(
+            credentials.password as string,
+            user.hashedPassword
+          );
+          if (!isMatch) {
+            throw new Error("Incorrect password.");
+          }
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
+
+const signIn: NextAuthResult["signIn"] = nextAuth.signIn;
+const auth: NextAuthResult["auth"] = nextAuth.auth;
+const handlers = nextAuth.handlers;
+const signOut = nextAuth.signOut;
+
+export { handlers, auth, signIn, signOut };
